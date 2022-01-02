6b440c1119bc2e4fa569b5f6ada08c7f0db4c07d
nehhhhh <92771013+nehhhhh@users.noreply.github.com>
2022-01-02 12:47:10 +0800

    Add files via upload

---
+++ c-client1.py
@@ -0,0 +1,30 @@
+import socket
+import signal
+import sys
+
+ClientSocket = socket.socket()
+host = '192.168.56.106'
+port = 8828
+
+print('Waiting for the connection')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+Response = ClientSocket.recv(1024)
+print(Response.decode("utf-8"))
+while True:
+    Input = input('\nLOG|SQUAREROOT|EXPONENT\nEnter exit to exit the process\nChoose the operation and insert a number: ')
+
+    if Input == 'exit':
+        break
+    else:
+        ClientSocket.send(str.encode(Input))
+        Response = ClientSocket.recv(1024)
+        print(Response.decode("utf-8"))
+
+ClientSocket.close()
+
+
+
